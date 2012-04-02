a12dcada54e90722fc4f89021af0defdae759093
César <cesar.bmcosta@gmail.com>
2012-04-02 16:49:41 -0300

    add a method to set a decorator to subform

--- library/EasyBib/Form/SubForm/Decorator.php
+++ library/EasyBib/Form/SubForm/Decorator.php
@@ -69,4 +69,19 @@ class EasyBib_Form_SubDecorator extends EasyBib_Form_Decorator
             'FormElements',
         )
     );
+    
+     /**
+      * Set the subform decorators by the given string format or by the default div style
+      *
+      * @param object $objSubForm        Zend_Form_SubForm pointer-reference
+      * @param string $constFormat    Project_Plugin_FormDecoratorDefinition constants
+      * @return NULL
+      */
+     public static function setFormDecorator(Zend_Form_SubForm $form, $format = self::BOOTSTRAP, $submit_str = 'submit', $cancel_str = 'cancel') {
+         parent::setFormDecorator($form, $format, $submit_str, $cancel_str);
+         $form->setDecorators(self::$_FormDecorator[$format]);
+ 
+     }
+ }
+
 }
